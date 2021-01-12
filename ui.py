d69739ec56435bc04d17a0a226b13eb58cf57061
ThibLi <thibault.liaigre@gmail.com>
2021-01-12 15:46:28 +0100

    12/01/2021 15h46

---
+++ ui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# creer une première fenetre
+window = Tk()
+
+# personnaliser cette fenetre
+window.title("Page d'accueil")
+window.geometry("1080x720")
+
+# afficher
+window.mainloop()
+
