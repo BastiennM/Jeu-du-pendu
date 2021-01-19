725bfa7fcb5e1bd7f933a6e13d72a026d44731a4
octalix <bastienmetais79@gmail.com>
2021-01-19 15:36:18 +0100

    19/01/2021h 15h36

--- ui.py
+++ ui.py
@@ -1,4 +1,16 @@
 from tkinter import *
+import tkinter
+
+#fonction
+
+
+def quitter():
+    fenetre.quit()
+
+def show_jeu():
+    jeu_window = tkinter.Toplevel(fenetre)
+    jeu_window.title("Jeu du pendu")
+
 
 # creer une première fenetre
 fenetre = Tk()
@@ -10,7 +22,21 @@ fenetre.minsize(480, 360)
 fenetre.iconbitmap("logo.ico")
 fenetre.config(background='#FF5733')
 
-#creer la frame
+#Widgets*
+
+mainmenu= tkinter.Menu(fenetre)
+first_menu = tkinter.Menu(mainmenu, tearoff=0)
+first_menu.add_command(label="Top 10")
+first_menu.add_command(label="Gestion des mots")
+first_menu.add_command(label="Aide", command=show_jeu)
+first_menu.add_command(label="Quitter", command=quitter)
+
+mainmenu.add_cascade(label="Menu", menu=first_menu)
+
+#boucle principale
+fenetre.config(menu=mainmenu)
+
+# creer la frame
 frame = Frame(fenetre, bg='#ff5733')
 
 # ajouter un premier texte
@@ -22,23 +48,22 @@ label_subtitle = Label(frame, text="PENDU", font=("Courrier", 40), bg="#FF5733",
 label_subtitle.pack(expand=YES)
 
 # ajouter un bouton
-menu_button = Button(frame, text="Jouer",height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button = Button(frame, text="Jouer", height="2", font=("Arial", 15), bg="white", fg="#FF5733")
 menu_button.pack(pady="15", fill=X)
 
-menu_button = Button(frame, text="Top 10",height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button = Button(frame, text="Top 10", height="2", font=("Arial", 15), bg="white", fg="#FF5733")
 menu_button.pack(pady="15", fill=X)
 
 menu_button = Button(frame, text="Gestion des mots", height="2", font=("Arial", 15), bg="white", fg="#FF5733")
 menu_button.pack(pady="15", fill=X)
 
-menu_button = Button(frame, text="Aide",height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button = Button(frame, text="Aide", height="2", font=("Arial", 15), bg="white", fg="#FF5733")
 menu_button.pack(pady="15", fill=X)
 
-menu_button = Button(frame, text="Quitter",height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button = Button(frame, text="Quitter", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=quitter)
 menu_button.pack(pady="15", fill=X)
 
-#ajouter
+# ajouter
 frame.pack(expand=YES)
 # afficher
 fenetre.mainloop()
-
