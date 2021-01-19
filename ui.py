b7072825f27fe23aa1b03f5c50d47930cb5dab19
octalix <bastienmetais79@gmail.com>
2021-01-19 14:40:34 +0100

    19/01/2021 14h40

--- ui.py
+++ ui.py
@@ -1,29 +1,44 @@
 from tkinter import *
-import tkinter.font as tkfont
+
 # creer une première fenetre
-window = Tk()
+fenetre = Tk()
 
 # personnaliser cette fenetre
-window.title("Page d'accueil")
-window.geometry("1080x720")
-window.minsize(1080, 720)
-window.iconbitmap("logo.ico")
-window.config(background='black')
+fenetre.title("Page d'accueil")
+fenetre.geometry("1080x720")
+fenetre.minsize(480, 360)
+fenetre.iconbitmap("logo.ico")
+fenetre.config(background='#FF5733')
+
+#creer la frame
+frame = Frame(fenetre, bg='#ff5733')
 
 # ajouter un premier texte
-label_title = Label(window, text="Bienvenu sur le site", font=("Montserrat", 40), bg='black', fg='white')
+label_title = Label(frame, text="Bienvenue sur le jeu du", font=("Courrier", 30), bg="#FF5733", fg="white")
 label_title.pack(expand=YES)
 
+# ajouter un second texte
+label_subtitle = Label(frame, text="PENDU", font=("Courrier", 40), bg="#FF5733", fg="white")
+label_subtitle.pack(expand=YES)
+
 # ajouter un bouton
-bouton=Button(
-              text = "Liste des mots",
-              font=tkfont.Font("Montserrat", "16"),
-              activebackground='red',
-              width=30,
-              height=5)
-bouton.pack(expand=YES)
+menu_button = Button(frame, text="Jouer",height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button.pack(pady="15", fill=X)
 
+menu_button = Button(frame, text="Top 10",height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button.pack(pady="15", fill=X)
 
+menu_button = Button(frame, text="Gestion des mots", height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button.pack(pady="15", fill=X)
+
+menu_button = Button(frame, text="Aide",height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button.pack(pady="15", fill=X)
+
+menu_button = Button(frame, text="Quitter",height="2", font=("Arial", 15), bg="white", fg="#FF5733")
+menu_button.pack(pady="15", fill=X)
+
+#ajouter
+frame.pack(expand=YES)
 # afficher
-window.mainloop()
+fenetre.mainloop()
 
