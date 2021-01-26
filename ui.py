e0e00d87712ed4b81f0e68b8ddfefd1716258fab
octalix <bastienmetais79@gmail.com>
2021-01-26 12:53:22 +0100

    25/01/2021 12h53

--- ui.py
+++ ui.py
@@ -22,7 +22,7 @@ def show_jeu():
     jeu_window.geometry("1080x720")
     jeu_window.minsize(480, 360)
     jeu_window.iconbitmap("img/logo.ico")
-    jeu_window.config(background='#FF5733')
+    jeu_window.config(background='#f9791e')
     acceuil.withdraw()
 
     # Menu Page de jeu
@@ -45,7 +45,7 @@ def show_top10():
     top10_window.geometry("1080x720")
     top10_window.minsize(480, 360)
     top10_window.iconbitmap("img/logo.ico")
-    top10_window.config(background='#FF5733')
+    top10_window.config(background='#f9791e')
     acceuil.withdraw()
 
     # Menu Page de jeu
@@ -67,7 +67,7 @@ def show_gestion():
     gestion_window.geometry("1080x720")
     gestion_window.minsize(480, 360)
     gestion_window.iconbitmap("img/logo.ico")
-    gestion_window.config(background='#FF5733')
+    gestion_window.config(background=' #f9791e')
     acceuil.withdraw()
 
     # Menu Page de jeu
@@ -89,7 +89,7 @@ def show_aide():
     aide_window.geometry("1080x720")
     aide_window.minsize(480, 360)
     aide_window.iconbitmap("img/logo.ico")
-    aide_window.config(background='#FF5733')
+    aide_window.config(background='#f9791e')
     acceuil.withdraw()
 
     # Menu Page de jeu
@@ -109,10 +109,12 @@ def show_aide():
 # <======================================================== PAGE ACCEUIL ========================================================>
 acceuil = Tk()
 acceuil.title("Page d'accueil")
-acceuil.geometry("1920x1080")
+acceuil.geometry("1024x768")
 acceuil.minsize(480, 360)
 acceuil.iconbitmap("img/logo.ico")
-acceuil.config(background='#FF5733')
+bg_acceuil = PhotoImage(file="img/acceuil.png")
+label_acceuil = Label(acceuil, image=bg_acceuil)
+label_acceuil.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Menu acceuil
 mainmenu = tkinter.Menu(acceuil)
@@ -125,35 +127,33 @@ mainmenu.add_cascade(label="Menu", menu=first_menu)
 acceuil.config(menu=mainmenu)
 
 # CRÉATION FRAME
-frame = Frame(acceuil, bg='#ff5733')
+frame_acceuiltop = Frame(acceuil)
+frame_acceuilbuttonplay= Frame(acceuil)
 
-# TEXTE
-label_title = Label(frame, text="Bienvenue sur le jeu du", font=("Courrier", 30), bg="#FF5733", fg="white")
+
+# # TEXTE
+label_title = Label(frame_acceuiltop, text="Bienvenue sur le jeu du", font=("Courrier", 30), bg="#FF5733", fg="white")
 label_title.pack()
-label_subtitle = Label(frame, text="PENDU", font=("Courrier", 40), bg="#FF5733", fg="white")
+label_subtitle = Label(frame_acceuiltop, text="PENDU", font=("Courrier", 40), bg="#FF5733", fg="white")
 label_subtitle.pack()
 
-# BUTTONS
-menu_button = Button(frame, text="Jouer", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=show_jeu)
+ # BUTTONS
+menu_button = Button(frame_acceuilbuttonplay, text="Jouer", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=show_jeu)
+menu_button.pack()
+menu_button = Button(frame_acceuiltop, text="Top 10", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=show_top10)
 menu_button.pack(pady="15", fill=X)
-menu_button = Button(frame, text="Top 10", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=show_top10)
+menu_button = Button(frame_acceuiltop, text="Gestion des mots", height="2", font=("Arial", 15), bg="white", fg="#FF5733",command=show_gestion)
 menu_button.pack(pady="15", fill=X)
-menu_button = Button(frame, text="Gestion des mots", height="2", font=("Arial", 15), bg="white", fg="#FF5733",command=show_gestion)
+menu_button = Button(frame_acceuiltop, text="Aide", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=show_aide)
 menu_button.pack(pady="15", fill=X)
-menu_button = Button(frame, text="Aide", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=show_aide)
-menu_button.pack(pady="15", fill=X)
-menu_button = Button(frame, text="Quitter", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=quitter)
+enu_button = Button(frame_acceuiltop, text="Quitter", height="2", font=("Arial", 15), bg="white", fg="#FF5733", command=quitter)
 menu_button.pack(pady="15", fill=X)
 
 # CANVAS
 
-# canvas = Canvas(width=400, height=250, bg="blue")
-# canvas.pack(pady=20)
-#
-# photoacceuil = PhotoImage(file="img/acceuil.png")
-# canvas.create_image(200, 25, image=photoacceuil, anchor=N)
 
 # AFFICHAGE FRAME
-frame.pack(expand=YES)
+frame_acceuiltop.place(x=30,y=10,width=960, height=115)
+frame_acceuilbuttonplay.place(x=365,y=190,width=310, height=40)
 # AFFICHAGE PAGE ACCEUIL
 acceuil.mainloop()
