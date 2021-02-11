b91daea23d8c415aac9497f8571ce76dd1fbcc33
BastosM <bastienmetais79@gmail.com>
2021-02-11 22:43:16 +0100

    Ajout top10 suppr si plus de 10 personne dans le top

--- top10.py
+++ top10.py
@@ -38,7 +38,7 @@ def opentop10():
                 listejoueur[len(listejoueur) - 1].append(nom)
                 listejoueur[len(listejoueur) - 1].append(diff)
                 listejoueur[len(listejoueur) - 1].append(score)
-            print(listejoueur)
+            print(len(listejoueur))
 
             def Sort(sub_li):
                 sub_li.sort(key=lambda x: x[2])
@@ -54,9 +54,10 @@ def opentop10():
             scrollbar.pack(side=RIGHT, fill=Y)
             frame_scrollist.pack()
             listbox.pack(pady=15)
-
             Sort(listejoueur)
-            print(listejoueur)
+            while len(listejoueur) > 10:
+                listejoueur.pop()
+            print(len(listejoueur))
             for item in listejoueur:
                 global cptinsert
                 listbox.insert(0, listejoueur[cptinsert])
